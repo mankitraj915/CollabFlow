08ed8c1ae94d02d5433689a0fce770858d843fe7
Ankit Raj <mankitraj915@gmail.com>
2026-06-18 16:36:53 +0530

    fix(deploy): install python dependencies in build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+echo "Installing dependencies..."
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
